4de807b74a07664fe83b096d8d468cbd5797304e
Giulia Gallo <giulia.arche@gmail.com>
2023-05-19 19:48:06 -0700

    Update convert_aks_utility.py

--- user_deployment_assets/RayCluster/convert_aks_utility.py
+++ user_deployment_assets/RayCluster/convert_aks_utility.py
@@ -1,5 +1,5 @@
 # Next run the following commands to encode(this assumes a fresh state)
-# This is autility for Key Vault
+# This is a utility for Key Vault
 import base64
 encoded = base64.b64encode(b'YOUR_APP_SECRET')
 
